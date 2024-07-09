76a0d584542132dd1930c6a9e99739779dc873c3
tanish_repositry <yesebu.10@gmail.com>
2024-07-09 22:59:59 +0530

    Added DTO files

---
+++ src/main/java/com/example/rest/templte/webservices/dto/Product.java
@@ -0,0 +1,21 @@
+package com.example.rest.templte.webservices.dto;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private double price;
+
+
+}
